8d042e1048019817f2dc0832f9156685294da6b1
clare-jisc <112627034+clare-jisc@users.noreply.github.com>
2024-06-18 13:00:55 +0100

    add image field to blog_detail_page model

--- blogpages/models.py
+++ blogpages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.images import get_image_model
 
 # Create your models here.
 class BlogIndex(Page):
@@ -41,7 +41,16 @@ class BlogDetail(Page):
     subtitle = models.CharField(max_length=100, blank=True, null=True)
     body = RichTextField(blank=True)
 
+    image = models.ForeignKey(
+        get_image_model(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
         FieldPanel("body"),
+        FieldPanel("image"),
     ]
